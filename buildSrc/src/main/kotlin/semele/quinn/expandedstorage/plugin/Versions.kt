af4d3c0689984ae440d27cb2eec08a08b26c111d
Quinn Semele <quinnsemele@duck.com>
2024-04-06 14:06:39 +0100

    Prep for release.

--- buildSrc/src/main/kotlin/semele/quinn/expandedstorage/plugin/Versions.kt
+++ buildSrc/src/main/kotlin/semele/quinn/expandedstorage/plugin/Versions.kt
@@ -3,7 +3,7 @@ package semele.quinn.expandedstorage.plugin
 import org.gradle.api.JavaVersion
 
 object Versions {
-    const val EXPANDEDSTORAGE = "10.3.0-beta.1"
+    const val EXPANDEDSTORAGE = "10.3.0-beta.2"
 
     // Generic
     val java = JavaVersion.VERSION_17
