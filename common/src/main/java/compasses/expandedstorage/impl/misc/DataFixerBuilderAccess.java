ed29e5e7365b1a8a8b7213b7af1393ffb3b4cb94
Quinn Semele <quinnsemele@duck.com>
2024-04-14 15:02:24 +0100

    Rollback mixin changes.

--- common/src/main/java/compasses/expandedstorage/impl/misc/DataFixerBuilderAccess.java
+++
@@ -1,7 +0,0 @@
-package compasses.expandedstorage.impl.misc;
-
-import com.mojang.datafixers.schemas.Schema;
-
-public interface DataFixerBuilderAccess {
-    Schema expandedstorage$getSchema(int version, int subversion);
-}
