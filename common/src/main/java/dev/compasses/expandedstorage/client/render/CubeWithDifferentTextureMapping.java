5683e518bdb00801ba81bc050bb95d133b24c9c1
Quinn Semele <quinn@compasses.dev>
2024-08-19 14:03:01 +0100

    Extract and name vertices

--- common/src/main/java/dev/compasses/expandedstorage/client/render/CubeWithDifferentTextureMapping.java
+++ common/src/main/java/dev/compasses/expandedstorage/client/render/CubeWithDifferentTextureMapping.java
@@ -2,7 +2,6 @@ package dev.compasses.expandedstorage.client.render;
 
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.core.Direction;
-import org.joml.Vector3f;
 
 import java.util.Set;
 
@@ -10,16 +9,32 @@ public class CubeWithDifferentTextureMapping extends ModelPart.Cube {
     public CubeWithDifferentTextureMapping(int texCoordU, int texCoordV, int texWidth, int texHeight, float originX, float originY, float originZ, float dimensionX, float dimensionY, float dimensionZ, Set<Direction> visibleFaces) {
         super(texCoordU, texCoordV, originX, originY, originZ, dimensionX, dimensionY, dimensionZ, 0, 0, 0, false, texWidth, texHeight, visibleFaces);
 
+        // Antipode means direct opposite, usually used in relation to spheres.
+        // Using it here to refer to (maxX, maxY, maxZ), opposite of the origin.
+        float antipodeX = originX + dimensionX;
+        float antipodeZ = originZ + dimensionZ;
+        float antipodeY = originY + dimensionY;
+        
+        // south = positiveZ = forward, east = positiveX = left ?
+        var origin = new ModelPart.Vertex(originX, originY, originZ, 0, 0);
+        var originForward = new ModelPart.Vertex(originX, originY, antipodeZ, 0, 0);
+        var originLeft = new ModelPart.Vertex(antipodeX, originY, originZ, 0, 0);
+        var originAcross = new ModelPart.Vertex(antipodeX, originY, antipodeZ, 0, 0);
+
+        var antipodeAcross = new ModelPart.Vertex(originX, antipodeY, originZ, 0, 0);
+        var antipodeRight = new ModelPart.Vertex(originX, antipodeY, antipodeZ, 0, 0);
+        var antipodeBackward = new ModelPart.Vertex(antipodeX, antipodeY, originZ, 0, 0);
+        var antipode = new ModelPart.Vertex(antipodeX, antipodeY, antipodeZ, 0, 0);
+
         int i = 0;
-        polygons = new ModelPart.Polygon[visibleFaces.size()];
 
         if (visibleFaces.contains(Direction.DOWN)) {
             polygons[i++] = new ModelPart.Polygon(
                     new ModelPart.Vertex[]{
-                            new ModelPart.Vertex(new Vector3f(originX + dimensionX, originY, originZ), 0, 0),
-                            new ModelPart.Vertex(new Vector3f(originX, originY, originZ), 0, 0),
-                            new ModelPart.Vertex(new Vector3f(originX, originY, originZ + dimensionZ), 0, 0),
-                            new ModelPart.Vertex(new Vector3f(originX + dimensionX, originY, originZ + dimensionZ), 0, 0),
+                            originLeft,
+                            origin,
+                            originForward,
+                            originAcross,
                     },
                     texCoordU + dimensionZ + dimensionX,
                     texCoordV,
@@ -35,10 +50,10 @@ public class CubeWithDifferentTextureMapping extends ModelPart.Cube {
         if (visibleFaces.contains(Direction.UP)) {
             polygons[i++] = new ModelPart.Polygon(
                     new ModelPart.Vertex[]{
-                            new ModelPart.Vertex(new Vector3f(originX + dimensionX, originY + dimensionY, originZ), 0, 0),
-                            new ModelPart.Vertex(new Vector3f(originX, originY + dimensionY, originZ), 0, 0),
-                            new ModelPart.Vertex(new Vector3f(originX, originY + dimensionY, originZ + dimensionZ), 0, 0),
-                            new ModelPart.Vertex(new Vector3f(originX + dimensionX, originY + dimensionY, originZ + dimensionZ), 0, 0),
+                            antipodeBackward,
+                            antipodeAcross,
+                            antipodeRight,
+                            antipode,
                     },
                     texCoordU + dimensionZ,
                     texCoordV,
@@ -54,10 +69,10 @@ public class CubeWithDifferentTextureMapping extends ModelPart.Cube {
         if (visibleFaces.contains(Direction.WEST)) {
             polygons[i++] = new ModelPart.Polygon(
                     new ModelPart.Vertex[]{
-                            new ModelPart.Vertex(new Vector3f(originX, originY + dimensionY, originZ), 0, 0),
-                            new ModelPart.Vertex(new Vector3f(originX, originY + dimensionY, originZ + dimensionZ), 0, 0),
-                            new ModelPart.Vertex(new Vector3f(originX, originY, originZ + dimensionZ), 0, 0),
-                            new ModelPart.Vertex(new Vector3f(originX, originY, originZ), 0, 0)
+                            antipodeAcross,
+                            antipodeRight,
+                            originForward,
+                            origin
                     },
                     texCoordU + dimensionZ,
                     texCoordV + dimensionZ,
@@ -73,10 +88,10 @@ public class CubeWithDifferentTextureMapping extends ModelPart.Cube {
         if (visibleFaces.contains(Direction.EAST)) {
             polygons[i++] = new ModelPart.Polygon(
                     new ModelPart.Vertex[]{
-                            new ModelPart.Vertex(new Vector3f(originX + dimensionX, originY + dimensionY, originZ), 0, 0),
-                            new ModelPart.Vertex(new Vector3f(originX + dimensionX, originY + dimensionY, originZ + dimensionZ), 0, 0),
-                            new ModelPart.Vertex(new Vector3f(originX + dimensionX, originY, originZ + dimensionZ), 0, 0),
-                            new ModelPart.Vertex(new Vector3f(originX + dimensionX, originY, originZ), 0, 0),
+                            antipodeBackward,
+                            antipode,
+                            originAcross,
+                            originLeft,
                     },
                     texCoordU + dimensionZ + dimensionX,
                     texCoordV + dimensionZ,
@@ -92,10 +107,10 @@ public class CubeWithDifferentTextureMapping extends ModelPart.Cube {
         if (visibleFaces.contains(Direction.SOUTH)) {
             polygons[i++] = new ModelPart.Polygon(
                     new ModelPart.Vertex[]{
-                            new ModelPart.Vertex(new Vector3f(originX, originY + dimensionY, originZ), 0, 0),
-                            new ModelPart.Vertex(new Vector3f(originX + dimensionX, originY + dimensionY, originZ), 0, 0),
-                            new ModelPart.Vertex(new Vector3f(originX + dimensionX, originY, originZ), 0, 0),
-                            new ModelPart.Vertex(new Vector3f(originX, originY, originZ), 0, 0),
+                            antipodeAcross,
+                            antipodeBackward,
+                            originLeft,
+                            origin,
                     },
                     texCoordU + dimensionZ + dimensionX + dimensionZ,
                     texCoordV + dimensionZ,
@@ -111,10 +126,10 @@ public class CubeWithDifferentTextureMapping extends ModelPart.Cube {
         if (visibleFaces.contains(Direction.NORTH)) {
             polygons[i++] = new ModelPart.Polygon(
                     new ModelPart.Vertex[]{
-                            new ModelPart.Vertex(new Vector3f(originX + dimensionX, originY + dimensionY, originZ + dimensionZ), 0, 0),
-                            new ModelPart.Vertex(new Vector3f(originX, originY + dimensionY, originZ + dimensionZ), 0, 0),
-                            new ModelPart.Vertex(new Vector3f(originX, originY, originZ + dimensionZ), 0, 0),
-                            new ModelPart.Vertex(new Vector3f(originX + dimensionX, originY, originZ + dimensionZ), 0, 0),
+                            antipode,
+                            antipodeRight,
+                            originForward,
+                            originAcross,
                     },
                     texCoordU + dimensionZ,
                     texCoordV + dimensionZ,
